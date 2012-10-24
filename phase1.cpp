d0adbc077b0612012eee02652fd0fb6d2b6208de
Tom Gorski <tom@gorskipro.com>
2012-10-24 17:28:11 -0400

    CPP Fix

---
+++ phase1.cpp
@@ -0,0 +1,4 @@
+//Course: 4002-208 Intro to Programming
+//Assignment: Project 5 - Chutes and Ladders - Phase 1
+//Group: Tom Gorski, Nathan Lichtenstein, William Olszewski
+//Phase 1: Calculate the number of rolls to land exactly on square 100.
