f1cf7ef2e94fb0da931332e48283b411cd722d7a
Nathan J. Lichtenstein <njlichtenstein@gmail.com>
2012-10-24 17:31:04 -0400

    I like Tom's Grandma's ASS

--- phase1.cpp
+++ phase1.cpp
@@ -2,3 +2,4 @@
 //Assignment: Project 5 - Chutes and Ladders - Phase 1
 //Group: Tom Gorski, Nathan Lichtenstein, William Olszewski
 //Phase 1: Calculate the number of rolls to land exactly on square 100.
+//I want to slap your grandma on her wrinkly ass <3
