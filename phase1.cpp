7a7503b5af9fefcb3631a2a421917ac681934e7d
Nathan J. Lichtenstein <njlichtenstein@gmail.com>
2012-10-24 17:35:26 -0400

    Began program
    Provided basic infrastructure for program, including libraries

--- phase1.cpp
+++ phase1.cpp
@@ -1,4 +1,17 @@
 //Course: 4002-208 Intro to Programming
 //Assignment: Project 5 - Chutes and Ladders - Phase 1
 //Group: Tom Gorski, Nathan Lichtenstein, William Olszewski
+
 //Phase 1: Calculate the number of rolls to land exactly on square 100.
+
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+{
+
+    return (0);
+}
