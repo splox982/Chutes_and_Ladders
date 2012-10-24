f4e29048646ba78a90795e0e2080ac702f7bd054
Tom Gorski <tom@gorskipro.com>
2012-10-24 17:32:34 -0400

    Removed dirty code.

--- phase1.cpp
+++ phase1.cpp
@@ -2,4 +2,3 @@
 //Assignment: Project 5 - Chutes and Ladders - Phase 1
 //Group: Tom Gorski, Nathan Lichtenstein, William Olszewski
 //Phase 1: Calculate the number of rolls to land exactly on square 100.
-//I want to slap your grandma on her wrinkly ass <3
